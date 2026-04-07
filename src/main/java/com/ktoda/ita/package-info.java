19de20efa65f8813eab167abf96bf8d405040cb3
Konstantin Todorov Andreev <ktod.andreev@gmail.com>
2026-04-07 20:15:30 +0300

    Add explanation of what is ita package

---
+++ src/main/java/com/ktoda/ita/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * ita is a package that follows the Introduction to Algorithms, 3rd Edition book
+ * <p>
+ * It provides, solutions, problem set exercises, and experiments
+ */
+package com.ktoda.ita;
